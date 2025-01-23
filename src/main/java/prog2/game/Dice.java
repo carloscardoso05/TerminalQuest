316733dae887bb81515cf5adcd7cb37f610cbbe5
thiago <thiago.csantanna04@gmail.com>
2025-01-23 11:22:20 -0300

    Classe Dice com métodos de rolagem de dados

--- src/main/java/prog2/game/Dice.java
+++ src/main/java/prog2/game/Dice.java
@@ -5,32 +5,34 @@ import java.util.Random;
 import prog2.entities.enums.ResultadoAtaque;
 
 public class Dice {
+    private static int roll(int numLados) {
+        Random rand = new Random();
+        return rand.nextInt(1, numLados + 1);
+    }
+
     public static int rollDice(int numLados, int bonus) {
         if (numLados < 2) {
             throw new IllegalArgumentException("Número de lados inválido");
         }
-        Random rand = new Random();
 
-        return rand.nextInt(1, numLados + 1) + bonus;
+        return roll(numLados) + bonus;
     }
     public static int rollDice(int numLados) {
         if (numLados < 2) {
             throw new IllegalArgumentException("Número de lados inválido");
         }
-        Random rand = new Random();
 
-        return rand.nextInt(1, numLados + 1);
+        return roll(numLados);
     }
 
     public static int rollDano(int numLados, int bonus, ResultadoAtaque resultado) {
         if (numLados < 2) {
             throw new IllegalArgumentException("Número de lados inválido");
         }
-        Random rand = new Random();
 
         return switch (resultado) {
-            case CRITICAL_HIT -> 2 * rand.nextInt(1, numLados + 1) + bonus;
-            case ACERTOU -> rand.nextInt(1, numLados + 1) + bonus;
+            case CRITICAL_HIT -> 2 * roll(numLados) + bonus;
+            case ACERTOU -> roll(numLados) + bonus;
             case ERROU -> 0;
         };
     }
@@ -41,8 +43,8 @@ public class Dice {
         Random rand = new Random();
 
         return switch (resultado) {
-            case CRITICAL_HIT -> 2 * rand.nextInt(1, numLados + 1);
-            case ACERTOU -> rand.nextInt(1, numLados + 1);
+            case CRITICAL_HIT -> 2 * roll(numLados);
+            case ACERTOU -> roll(numLados);
             case ERROU -> 0;
         };
     }
