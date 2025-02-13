2b0ddd2f0c2cd1fefcdef12c683dfb94e05ab03d
thiago <thiago.csantanna04@gmail.com>
2025-02-13 09:56:41 -0300

    Implementação da habilidade de chamar a atenção

--- src/main/java/prog2/entities/actions/skills/tanque/ChamarAtencao.java
+++ src/main/java/prog2/entities/actions/skills/tanque/ChamarAtencao.java
@@ -1,8 +1,9 @@
 package prog2.entities.actions.skills.tanque;
 
 import prog2.entities.Player;
-import prog2.entities.actions.Action;
+import prog2.entities.status.Ameacador;
 import prog2.entities.actions.skills.Skill;
+import prog2.entities.status.Ameacador;
 
 public class ChamarAtencao extends Skill {
     public ChamarAtencao() {
@@ -11,7 +12,7 @@ public class ChamarAtencao extends Skill {
 
     @Override
     public void execute(Player origem, Player[] alvos) {
-        // TODO: CRIAR ATRIBUTO MANA NA CLASSE PLAYER
-        // TODO: ADICIONAR ATRIBUTO DE AMEAÇA
+        origem.setManaAtual(origem.getManaAtual() - this.getCusto());
+        origem.getStatus().add(new Ameacador());
     }
 }
