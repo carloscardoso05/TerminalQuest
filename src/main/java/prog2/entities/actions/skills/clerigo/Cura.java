fe82437b3f1127e90f54e3616a239705ea23f3f3
thiago <thiago.csantanna04@gmail.com>
2025-02-13 09:56:50 -0300

    Implementação da habilidade de cura

--- src/main/java/prog2/entities/actions/skills/clerigo/Cura.java
+++ src/main/java/prog2/entities/actions/skills/clerigo/Cura.java
@@ -3,22 +3,22 @@ package prog2.entities.actions.skills.clerigo;
 import prog2.entities.Player;
 import prog2.entities.actions.skills.Skill;
 import prog2.game.Dice;
-import prog2.entities.actions.Action;
+
+import javax.naming.InsufficientResourcesException;
+import java.util.MissingResourceException;
 
 public class Cura extends Skill {
     private static final int MAX_CURA = 10;
-    private static final int CUSTO = 5;
 
     public Cura() {
-        super("Cura", CUSTO);
+        super("Cura", 5);
     }
 
     @Override
     public void execute(Player origem, Player[] alvos) {
         Player alvo = alvos[0];
 
-        // TODO: CRIAR ATRIBUTO MANA NA CLASSE PLAYER
-        // TODO: POSSIVELMENTE ADICIONAR ATRIBUTO DE MAGIA PARA BÔNUS
+        origem.setManaAtual(origem.getManaAtual() - this.getCusto());
         int cura = Dice.rollDice(MAX_CURA);
         alvo.setVidaAtual(Math.min(alvo.getVidaAtual() + cura, alvo.getVidaMaxima()));
     }
