96b1d0aadaf3a1ce5262611b7946337a8dda189d
thiago <thiago.csantanna04@gmail.com>
2025-02-18 09:45:31 -0300

    Implementação do método nextTurn da classe Turno

--- src/main/java/prog2/game/Turno.java
+++ src/main/java/prog2/game/Turno.java
@@ -1,16 +1,17 @@
 package prog2.game;
 
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import prog2.entities.Hero;
 import prog2.entities.Monster;
 import prog2.entities.Player;
+import prog2.entities.status.Status;
 import prog2.util.ToString;
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import prog2.util.exceptions.ImpedeAcao;
 
 public class Turno implements Serializable {
     @Serial
@@ -19,28 +20,79 @@ public class Turno implements Serializable {
     private int turnNumber;
 
     public void nextTurn() {
-        // Lógica de turno
+        if (turnNumber == 1) {
+            getPlayerEmOrdemDeAcao();
+        }
+        for (Player p : this.getPlayers()) {
+            // Aplica os efeitos de status e verifica possibilidade de ação
+            List<Status> status = p.getStatus();
+            boolean acao = true;
+            for (Status s : status) {
+                if (s.getDuracaoRestante() < 1) {
+                    s.removerEfeito(p);
+                    // Talvez quebre remover no meio da iteração
+                    status.remove(s);
+                    continue;
+                }
+                try {
+                    s.diminuirDuracaoRestante();
+                    s.aplicarEfeito(p);
+                } catch (ImpedeAcao e) {
+                    acao = false;
+                }
+            }
+            if (!acao) {
+                continue;
+            }
+
+            if (p instanceof Hero) {
+                // Get ação e alvo
+                int tipoAcao = 0;
+                Player[] alvos = null;
+                switch (tipoAcao) {
+                    case 0:
+                        p.getAtaque().execute(p, alvos);
+                        break;
+                    case 1:
+                        p.getHabilidades().get(0).execute(p, alvos);
+                        break;
+                    case 2:
+                        // ITEM
+                }
+            }
+            else {
+                // IA do monstro
+            }
+        }
         turnNumber += 1;
     }
 
-    private final Set<Player> players = new HashSet<>();
-
-    public Set<Player> getPlayers() {
-        return Set.copyOf(players);
+    public List<Player> getPlayerEmOrdemDeAcao() {
+        final List<Player> players = new ArrayList<>(getPlayers());
+        players.sort((p1, p2) -> Integer.compare(
+                Dice.rollDice(20, p2.getVelocidade()),
+                Dice.rollDice(20, p1.getVelocidade())));
+        return players;
     }
 
-    public Set<Monster> getMonsters() {
+    private final List<Player> players = new ArrayList<>();
+
+    public List<Player> getPlayers() {
+        return List.copyOf(players);
+    }
+
+    public List<Monster> getMonsters() {
         return getPlayers().stream()
                 .filter(p -> p instanceof Monster)
                 .map(p -> (Monster) p)
-                .collect(Collectors.toSet());
+                .collect(Collectors.toList());
     }
 
-    public Set<Hero> getHeroes() {
+    public List<Hero> getHeroes() {
         return getPlayers().stream()
                 .filter(p -> p instanceof Hero)
                 .map(p -> (Hero) p)
-                .collect(Collectors.toSet());
+                .collect(Collectors.toList());
     }
 
     public int getPlayersCount() {
@@ -57,6 +109,12 @@ public class Turno implements Serializable {
 
     public void addPlayer(Player player) {
         players.add(player);
+    }
+
+    public void addPlayer(List<Player> players) {
+        for (final Player player : players) {
+            addPlayer(player);
+        }
     }
 
     public int getTurnNumber() {
