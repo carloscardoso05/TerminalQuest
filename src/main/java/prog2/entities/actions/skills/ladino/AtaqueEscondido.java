346b763ac13c5993b24439381ea8c00c041c789b
Carlos Silva <cvcardos5@gmail.com>
2025-02-24 23:21:23 -0300

    corrige bugs

--- src/main/java/prog2/entities/actions/skills/ladino/AtaqueEscondido.java
+++ src/main/java/prog2/entities/actions/skills/ladino/AtaqueEscondido.java
@@ -21,7 +21,6 @@ public class AtaqueEscondido extends Skill {
         Escondido statusEscondido = new Escondido(alvo);
 //        Primeiro turno: se esconder
         if (!origem.getStatus().contains(statusEscondido)) {
-            this.checarMana(origem.getManaAtual(), origem);
             origem.setManaAtual(origem.getManaAtual() - this.getCusto());
             origem.getStatus().add(statusEscondido);
             return ResultadoAtaque.ERROU; // TODO rever isso aqui
